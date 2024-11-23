39622c4a0523234d55058f4f5d2b56d56b748631
Joe Bentley <joebentley10@gmail.com>
2024-11-23 10:15:04 +0100

    Example xy "texture"

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,10 @@ fn main() {
     let mut image = Image::new(400, 400);
 
     for y in 0..image.height as usize {
+        let g = y as f32 / image.height as f32;
         for x in 0..image.width as usize {
-            image.put_pixel(x, y, Colour::new(1., 0., 0.).as_rgb24());
+            let r = x as f32 / image.width as f32;
+            image.put_pixel(x, y, Colour::new(r, g, 0.).as_rgb24());
         }
     }
 
