58a281f2dc90dea4750022105d8b31dae3948f49
Joe Bentley <joebentley10@gmail.com>
2024-11-23 15:52:28 +0100

    Added length/normalised vector operations

--- src/vector.rs
+++ src/vector.rs
@@ -11,6 +11,26 @@ impl Vector {
     pub fn new(x: f64, y: f64, z: f64) -> Self {
         Self { x, y, z }
     }
+
+    pub fn abs(&self) -> f64 {
+        self.x * self.x + self.y * self.y + self.z * self.z
+    }
+
+    pub fn length(&self) -> f64 {
+        self.abs().sqrt()
+    }
+
+    pub fn normalise(&mut self) {
+        let length = self.length();
+        self.x /= length;
+        self.y /= length;
+        self.z /= length;
+    }
+
+    pub fn normalised(mut self) -> Self {
+        self.normalise();
+        return self;
+    }
 }
 
 impl Add for Vector {
@@ -69,5 +89,22 @@ mod tests {
         assert_eq!(a - b, Vector::new(-2., 0., 2.));
         a -= b;
         assert_eq!(a, Vector::new(-2., 0., 2.));
+    }
+
+    #[test]
+    fn vector_abs_and_length() {
+        let a = Vector::new(1., 2., 3.);
+        assert_eq!(a.abs(), 14.);
+        assert_eq!(a.length(), (14.0 as f64).sqrt());
+    }
+
+    #[test]
+    fn vector_normalisation() {
+        let mut a = Vector::new(1., 2., 3.);
+        let length = a.length();
+        let a_normalised = a.normalised();
+        assert_eq!(a_normalised.x, 1. / length);
+        a.normalise();
+        assert_eq!(a.y, 2. / length);
     }
 }
